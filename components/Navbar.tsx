e3366fe0e8744e2255c36a51963adfb4760a66b6
Kody Deda <kodydeda4@gmail.com>
2024-04-23 10:19:56 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-10 bg-gray-400 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
+    <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <span className="text-2xl text-gray-900 font-semibold">
@@ -58,18 +58,31 @@ export default function Navbar() {
             </ul>
           </span>
           <div className="flex space-x-4 text-gray-900">
-            {/* Right Items */}
-            <ul class="flex gap-4">
-              <li>
-                <ModeToggle />
-              </li>
-              <li>
-                <SheetDemo />
-              </li>
-            </ul>
+            <a href="#">Dashboard</a>
+            <a href="#">About</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
           </div>
         </div>
       </div>
+    </nav>
+  )
+}
+
+
+export function NavbarFR() {
+  return (
+    <nav class="flex justify-between p-4">
+
+      {/* Right Items */}
+      <ul class="flex gap-4">
+        <li>
+          <ModeToggle />
+        </li>
+        <li>
+          <SheetDemo />
+        </li>
+      </ul>
     </nav>
   )
 }
