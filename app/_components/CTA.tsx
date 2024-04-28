9f1363e34961f35ec6a8e93f26197fceb184e83f
Kody Deda <kodydeda4@gmail.com>
2024-04-28 18:03:34 +0200

    clean

--- app/_components/CTA.tsx
+++ app/_components/CTA.tsx
@@ -6,10 +6,10 @@ export default function CTA() {
   return (
     <div className="flex flex-col w-full bg-neutral-500 bg-opacity-5 relative border-t">
       <div className="max-w-3xl mx-auto px-6 sm:px-12 my-8 py-4 mb-24 w-full">
-        <h2 className="text-3xl font-extrabold pb-2">
+        <h2 className="text-2xl font-extrabold pb-2">
           Contact Me!
         </h2>
-        <p className="text-xl text-muted-foreground pb-4">
+        <p className="text-lg text-muted-foreground pb-4">
           {"Let's start writing some code."}
         </p>
         <div className="flex gap-4">
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
     <div className="flex flex-col gap-4">
       <div className="bg-black w-full rounded-lg h-64 relative">
         <Image
-          src="/avatar-hero.png"
+          src={RESUME.srcHero}
           alt="Hero image."
           layout="fill"
           objectFit="contain"
@@ -16,14 +16,13 @@ export default function Hero() {
       </div>
       <div className="flex flex-col">
         <h1 className="text-2xl font-extrabold">
-          {/* Hi, I&apos;m {RESUME.name}. */}
           {RESUME.name}
         </h1>
         <h2 className="text-lg pb-2">
-          {" iOS Developer"}
+          {RESUME.profession}
         </h2>
         <p className="text pb-4">
-          {"Developer with a focus on building great-looking software experiences, application architecture, and all things Swift."}
+          {RESUME.description}
         </p>
         <div className="flex gap-4">
           <Link href={`mailto:${RESUME.email}`} className="w-full">
@@ -37,5 +36,6 @@ export default function Hero() {
     </div >
   );
 }
+
 
 
