ad4cadebd6776f547e9c5ba92116c6dc921033df
Kody Deda <kodydeda4@gmail.com>
2025-01-29 12:54:58 -0500

    Update Hero.tsx

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
 function ProfileHeader() {
   return (
     <div
-      className="relative h-48 w-full bg-cover bg-center  h-56 relative border-b"
+      className="relative h-48 w-full bg-cover bg-center h-40 sm:h-60 border-b"
       style={{ backgroundImage: `url(${RESUME.srcHero})` }}
     >
       {/* Position the avatar on top of the background */}
